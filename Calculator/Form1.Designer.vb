3e84d658df8199c26c45cf8e72da61e35af1c188
Almeeida <almeidx@pm.me>
2020-06-29 12:44:40 +0100

    first commit

---
+++ Calculator/Form1.Designer.vb
@@ -0,0 +1,305 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button0 = New System.Windows.Forms.Button()
+        Me.Dot = New System.Windows.Forms.Button()
+        Me.Result = New System.Windows.Forms.Button()
+        Me.Minus = New System.Windows.Forms.Button()
+        Me.Plus = New System.Windows.Forms.Button()
+        Me.Multiplication = New System.Windows.Forms.Button()
+        Me.Division = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.ParenthesesRight = New System.Windows.Forms.Button()
+        Me.ParenthesesLeft = New System.Windows.Forms.Button()
+        Me.Clear = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.SystemColors.ButtonShadow
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!)
+        Me.Button1.Location = New System.Drawing.Point(12, 69)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(66, 48)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "1"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.SystemColors.ButtonShadow
+        Me.Button2.Location = New System.Drawing.Point(84, 69)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(66, 48)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "2"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.SystemColors.ButtonShadow
+        Me.Button3.Location = New System.Drawing.Point(156, 69)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(66, 48)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "3"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Button4
+        '
+        Me.Button4.BackColor = System.Drawing.SystemColors.ButtonShadow
+        Me.Button4.Location = New System.Drawing.Point(12, 123)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(66, 48)
+        Me.Button4.TabIndex = 4
+        Me.Button4.Text = "4"
+        Me.Button4.UseVisualStyleBackColor = False
+        '
+        'Button5
+        '
+        Me.Button5.BackColor = System.Drawing.SystemColors.ButtonShadow
+        Me.Button5.Location = New System.Drawing.Point(84, 123)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(66, 48)
+        Me.Button5.TabIndex = 5
+        Me.Button5.Text = "5"
+        Me.Button5.UseVisualStyleBackColor = False
+        '
+        'Button6
+        '
+        Me.Button6.BackColor = System.Drawing.SystemColors.ButtonShadow
+        Me.Button6.Location = New System.Drawing.Point(156, 123)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(66, 48)
+        Me.Button6.TabIndex = 6
+        Me.Button6.Text = "6"
+        Me.Button6.UseVisualStyleBackColor = False
+        '
+        'Button7
+        '
+        Me.Button7.BackColor = System.Drawing.SystemColors.ButtonShadow
+        Me.Button7.Location = New System.Drawing.Point(12, 177)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(66, 48)
+        Me.Button7.TabIndex = 8
+        Me.Button7.Text = "7"
+        Me.Button7.UseVisualStyleBackColor = False
+        '
+        'Button8
+        '
+        Me.Button8.BackColor = System.Drawing.SystemColors.ButtonShadow
+        Me.Button8.Location = New System.Drawing.Point(84, 177)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(66, 48)
+        Me.Button8.TabIndex = 9
+        Me.Button8.Text = "8"
+        Me.Button8.UseVisualStyleBackColor = False
+        '
+        'Button9
+        '
+        Me.Button9.BackColor = System.Drawing.SystemColors.ButtonShadow
+        Me.Button9.Location = New System.Drawing.Point(156, 177)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(66, 48)
+        Me.Button9.TabIndex = 10
+        Me.Button9.Text = "9"
+        Me.Button9.UseVisualStyleBackColor = False
+        '
+        'Button0
+        '
+        Me.Button0.BackColor = System.Drawing.SystemColors.ButtonShadow
+        Me.Button0.Location = New System.Drawing.Point(12, 231)
+        Me.Button0.Name = "Button0"
+        Me.Button0.Size = New System.Drawing.Size(66, 48)
+        Me.Button0.TabIndex = 12
+        Me.Button0.Text = "0"
+        Me.Button0.UseVisualStyleBackColor = False
+        '
+        'Dot
+        '
+        Me.Dot.BackColor = System.Drawing.SystemColors.ButtonShadow
+        Me.Dot.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!)
+        Me.Dot.Location = New System.Drawing.Point(84, 231)
+        Me.Dot.Name = "Dot"
+        Me.Dot.Size = New System.Drawing.Size(66, 48)
+        Me.Dot.TabIndex = 13
+        Me.Dot.Text = "."
+        Me.Dot.UseVisualStyleBackColor = False
+        '
+        'Result
+        '
+        Me.Result.BackColor = System.Drawing.SystemColors.ButtonShadow
+        Me.Result.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!)
+        Me.Result.Location = New System.Drawing.Point(156, 231)
+        Me.Result.Name = "Result"
+        Me.Result.Size = New System.Drawing.Size(66, 102)
+        Me.Result.TabIndex = 14
+        Me.Result.Text = "="
+        Me.Result.UseVisualStyleBackColor = False
+        '
+        'Minus
+        '
+        Me.Minus.BackColor = System.Drawing.SystemColors.ButtonShadow
+        Me.Minus.Location = New System.Drawing.Point(228, 231)
+        Me.Minus.Name = "Minus"
+        Me.Minus.Size = New System.Drawing.Size(66, 48)
+        Me.Minus.TabIndex = 15
+        Me.Minus.Text = "-"
+        Me.Minus.UseVisualStyleBackColor = False
+        '
+        'Plus
+        '
+        Me.Plus.BackColor = System.Drawing.SystemColors.ButtonShadow
+        Me.Plus.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!)
+        Me.Plus.Location = New System.Drawing.Point(228, 285)
+        Me.Plus.Name = "Plus"
+        Me.Plus.Size = New System.Drawing.Size(66, 48)
+        Me.Plus.TabIndex = 18
+        Me.Plus.Text = "+"
+        Me.Plus.UseVisualStyleBackColor = False
+        '
+        'Multiplication
+        '
+        Me.Multiplication.BackColor = System.Drawing.SystemColors.ButtonShadow
+        Me.Multiplication.Location = New System.Drawing.Point(228, 177)
+        Me.Multiplication.Name = "Multiplication"
+        Me.Multiplication.Size = New System.Drawing.Size(66, 48)
+        Me.Multiplication.TabIndex = 11
+        Me.Multiplication.Text = "*"
+        Me.Multiplication.UseVisualStyleBackColor = False
+        '
+        'Division
+        '
+        Me.Division.BackColor = System.Drawing.SystemColors.ButtonShadow
+        Me.Division.Location = New System.Drawing.Point(228, 123)
+        Me.Division.Name = "Division"
+        Me.Division.Size = New System.Drawing.Size(66, 48)
+        Me.Division.TabIndex = 7
+        Me.Division.Text = "/"
+        Me.Division.UseVisualStyleBackColor = False
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 27)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(283, 22)
+        Me.TextBox1.TabIndex = 19
+        '
+        'ParenthesesRight
+        '
+        Me.ParenthesesRight.BackColor = System.Drawing.SystemColors.ButtonShadow
+        Me.ParenthesesRight.Location = New System.Drawing.Point(84, 285)
+        Me.ParenthesesRight.Name = "ParenthesesRight"
+        Me.ParenthesesRight.Size = New System.Drawing.Size(66, 48)
+        Me.ParenthesesRight.TabIndex = 17
+        Me.ParenthesesRight.Text = ")"
+        Me.ParenthesesRight.UseVisualStyleBackColor = False
+        '
+        'ParenthesesLeft
+        '
+        Me.ParenthesesLeft.BackColor = System.Drawing.SystemColors.ButtonShadow
+        Me.ParenthesesLeft.Location = New System.Drawing.Point(11, 285)
+        Me.ParenthesesLeft.Name = "ParenthesesLeft"
+        Me.ParenthesesLeft.Size = New System.Drawing.Size(66, 48)
+        Me.ParenthesesLeft.TabIndex = 16
+        Me.ParenthesesLeft.Text = "("
+        Me.ParenthesesLeft.UseVisualStyleBackColor = False
+        '
+        'Clear
+        '
+        Me.Clear.BackColor = System.Drawing.SystemColors.ButtonShadow
+        Me.Clear.Location = New System.Drawing.Point(228, 69)
+        Me.Clear.Name = "Clear"
+        Me.Clear.Size = New System.Drawing.Size(66, 48)
+        Me.Clear.TabIndex = 3
+        Me.Clear.Text = "C"
+        Me.Clear.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.AppWorkspace
+        Me.ClientSize = New System.Drawing.Size(306, 351)
+        Me.Controls.Add(Me.Clear)
+        Me.Controls.Add(Me.ParenthesesLeft)
+        Me.Controls.Add(Me.ParenthesesRight)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Division)
+        Me.Controls.Add(Me.Multiplication)
+        Me.Controls.Add(Me.Plus)
+        Me.Controls.Add(Me.Minus)
+        Me.Controls.Add(Me.Result)
+        Me.Controls.Add(Me.Dot)
+        Me.Controls.Add(Me.Button0)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button0 As Button
+    Friend WithEvents Dot As Button
+    Friend WithEvents Result As Button
+    Friend WithEvents Minus As Button
+    Friend WithEvents Plus As Button
+    Friend WithEvents Multiplication As Button
+    Friend WithEvents Division As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents ParenthesesRight As Button
+    Friend WithEvents ParenthesesLeft As Button
+    Friend WithEvents Clear As Button
+End Class
---
+++ Calculator/Form1.vb
@@ -0,0 +1,145 @@
+﻿Public Class Form1
+    Dim exp As String = Nothing
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        exp = TextBox1.Text()
+        exp += "1"
+        UpdateText()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        exp = TextBox1.Text()
+        exp += "2"
+        UpdateText()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        exp = TextBox1.Text()
+        exp += "3"
+        UpdateText()
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        exp = TextBox1.Text()
+        exp += "4"
+        UpdateText()
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        exp = TextBox1.Text()
+        exp += "5"
+        UpdateText()
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        exp = TextBox1.Text()
+        exp += "6"
+        UpdateText()
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        exp = TextBox1.Text()
+        exp += "7"
+        UpdateText()
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        exp = TextBox1.Text()
+        exp += "8"
+        UpdateText()
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        exp = TextBox1.Text()
+        exp += "9"
+        UpdateText()
+    End Sub
+
+    Private Sub Button0_Click(sender As Object, e As EventArgs) Handles Button0.Click
+        exp = TextBox1.Text()
+        exp += "0"
+        UpdateText()
+    End Sub
+
+    Private Sub Dot_Click(sender As Object, e As EventArgs) Handles Dot.Click
+        exp = TextBox1.Text()
+        exp += "."
+        UpdateText()
+    End Sub
+
+    Private Sub Plus_Click(sender As Object, e As EventArgs) Handles Plus.Click
+        exp = TextBox1.Text()
+        exp += " + "
+        UpdateText()
+    End Sub
+
+    Private Sub Minus_Click(sender As Object, e As EventArgs) Handles Minus.Click
+        exp = TextBox1.Text()
+        exp += " - "
+        UpdateText()
+    End Sub
+
+    Private Sub Multiplication_Click(sender As Object, e As EventArgs) Handles Multiplication.Click
+        exp = TextBox1.Text()
+        exp += " * "
+        UpdateText()
+    End Sub
+
+    Private Sub Division_Click(sender As Object, e As EventArgs) Handles Division.Click
+        exp = TextBox1.Text()
+        exp += " / "
+        UpdateText()
+    End Sub
+
+    Private Sub ParenthesesLeft_Click(sender As Object, e As EventArgs) Handles ParenthesesLeft.Click
+        exp = TextBox1.Text()
+        exp += "("
+        UpdateText()
+    End Sub
+
+    Private Sub ParenthesesRight_Click(sender As Object, e As EventArgs) Handles ParenthesesRight.Click
+        exp = TextBox1.Text()
+        exp += ")"
+        UpdateText()
+    End Sub
+
+    Private Sub UpdateText()
+        TextBox1.Clear()
+        TextBox1.AppendText(exp)
+    End Sub
+
+    Private Sub Result_Click(sender As Object, e As EventArgs) Handles Result.Click
+        exp = TextBox1.Text()
+        Try
+            Dim res = New DataTable().Compute(exp, Nothing)
+            exp = Nothing
+            TextBox1.Clear()
+            TextBox1.AppendText(res)
+        Catch
+            exp = Nothing
+            TextBox1.Clear()
+            TextBox1.AppendText("ERROR")
+        End Try
+    End Sub
+
+    Private Sub Clear_Click(sender As Object, e As EventArgs) Handles Clear.Click
+        exp = Nothing
+        TextBox1.Clear()
+    End Sub
+
+    Private Sub TextBox1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBox1.KeyPress
+        If e.KeyChar = ChrW(Keys.Return) Then
+            exp = TextBox1.Text()
+            Try
+                Dim res = New DataTable().Compute(exp, Nothing)
+                exp = Nothing
+                TextBox1.Clear()
+                TextBox1.AppendText(res)
+            Catch
+                exp = Nothing
+                TextBox1.Clear()
+                TextBox1.AppendText("ERROR")
+            End Try
+        End If
+    End Sub
+End Class
